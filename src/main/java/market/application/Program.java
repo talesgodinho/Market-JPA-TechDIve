6912f7e416b65fc94a1392d7e9a4a62e26944ea5
Tales Godinho <talesflg@hotmail.com>
2022-03-16 21:05:31 -0300

    Adicionado logger

---
+++ src/main/java/market/application/Program.java
@@ -0,0 +1,14 @@
+package market.application;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Program {
+	
+	private static Logger LOG = LogManager.getLogger(Program.class);
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
